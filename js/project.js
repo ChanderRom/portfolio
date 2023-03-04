da61f37577acb6a29f60a03158ec1c9f05102903
ChaanChanChan <alexandertakepics@gmail.com>
2023-03-04 13:48:47 +0100

    accordion logic done, styles started

---
+++ js/project.js
@@ -0,0 +1,47 @@
+gsap.registerPlugin(ScrollTrigger)
+
+//TIMELINE OPENING ABOUT ME DESKTOP
+let mediaDeskProject = gsap.matchMedia()
+
+mediaDeskProject.add('(min-width: 680px', () => {
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.project-container',
+            star: 'top center',
+            end: 'center 60%',
+            scrub: 4,
+            toggleActions: 'restart none none reset'
+        }
+    })
+        .from('.project-title', {
+            y: -100,
+            opacity: 0,
+            ease: 'power3'
+        })
+        .from('.project-triangle', {
+            x: 200,
+            opacity: 0,
+            rotate: 300,
+            ease: 'power3'
+        })
+})
+
+
+// PROJECT ACCORDION
+let projects = gsap.utils.toArray(".project-container");
+
+projects.forEach(project => {
+    let paragraph = project.querySelector('.paragraph')
+    let subtitle = project.querySelector('.subtitle')
+    let subparagraph = project.querySelector('.subparagraph')
+
+    let tl = gsap.timeline({ paused: true })
+
+    tl.to(project, { duration: .8, height: 'auto', background: 'black', color: 'white', ease: 'power3' })
+
+    tl.reverse()
+
+    project.addEventListener("click", () => {
+        tl.reversed(!tl.reversed())
+    })
+})
